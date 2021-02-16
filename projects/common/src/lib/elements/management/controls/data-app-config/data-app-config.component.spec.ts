beb3702523e248b5c2a0b89b5a174a22de72b8c6
Jack Penhale <jack.penhale@fathym.com>
2021-02-16 14:43:52 -0700

    agular upgraded

--- projects/common/src/lib/elements/management/controls/data-app-config/data-app-config.component.spec.ts
+++ projects/common/src/lib/elements/management/controls/data-app-config/data-app-config.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DataAppConfigComponent } from './data-app-config.component';
 
@@ -6,7 +6,7 @@ describe('DataAppConfigComponent', () => {
   let component: DataAppConfigComponent;
   let fixture: ComponentFixture<DataAppConfigComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DataAppConfigComponent ]
     })
