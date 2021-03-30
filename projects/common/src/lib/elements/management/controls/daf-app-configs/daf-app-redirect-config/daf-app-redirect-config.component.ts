e2e51cf7302acda96035e4c56acf15153e055837
mcgear <michael.gearhardt@gmail.com>
2021-03-30 15:23:28 -0600

    Checkpoint

--- projects/common/src/lib/elements/management/controls/daf-app-configs/daf-app-redirect-config/daf-app-redirect-config.component.ts
+++ projects/common/src/lib/elements/management/controls/daf-app-configs/daf-app-redirect-config/daf-app-redirect-config.component.ts
@@ -5,16 +5,17 @@ import { DAFRedirectApplicationDetails } from '@lcu/common';
 @Component({
   selector: 'lcu-daf-app-redirect-config',
   templateUrl: './daf-app-redirect-config.component.html',
-  styleUrls: ['./daf-app-redirect-config.component.scss']
+  styleUrls: ['./daf-app-redirect-config.component.scss'],
 })
-export class DafAppRedirectConfigComponent implements  OnDestroy, OnInit {
+export class DafAppRedirectConfigComponent implements OnDestroy, OnInit {
   //  Fields
 
   //  Properties
   public get Config(): { [key: string]: any } {
-    debugger;
     return {
-      Redirect: this.FormGroup.controls.redirect.value
+      Permanent: this.FormGroup.controls.permanent.value,
+      PreserveMethod: this.FormGroup.controls.preserveMethod.value,
+      Redirect: this.FormGroup.controls.redirect.value,
     };
   }
 
@@ -24,6 +25,18 @@ export class DafAppRedirectConfigComponent implements  OnDestroy, OnInit {
   @Input('form-group')
   public FormGroup: FormGroup;
 
+  public get RedirectType(): string {
+    if (this.Config.Permanent && this.Config.PreserveMethod) {
+      return '308';
+    } else if (this.Config.Permanent && !this.Config.PreserveMethod) {
+      return '301';
+    } else if (!this.Config.Permanent && this.Config.PreserveMethod) {
+      return '307';
+    } else if (!this.Config.Permanent && !this.Config.PreserveMethod) {
+      return '302';
+    }
+  }
+
   //  Constructors
   constructor() {
     this.Details = {};
@@ -31,13 +44,33 @@ export class DafAppRedirectConfigComponent implements  OnDestroy, OnInit {
 
   //  Life Cycle
   public ngOnDestroy(): void {
+    this.FormGroup.removeControl('permanent');
+
+    this.FormGroup.removeControl('preserveMethod');
+
     this.FormGroup.removeControl('redirect');
   }
 
   public ngOnInit(): void {
     this.FormGroup.addControl(
+      'permanent',
+      new FormControl(!this.Details ? '' : this.Details.Permanent, [
+        Validators.required,
+      ])
+    );
+
+    this.FormGroup.addControl(
+      'preserveMethod',
+      new FormControl(!this.Details ? '' : this.Details.PreserveMethod, [
+        Validators.required,
+      ])
+    );
+
+    this.FormGroup.addControl(
       'redirect',
-      new FormControl(!this.Details ? '' : this.Details.Redirect, [Validators.required])
+      new FormControl(!this.Details ? '' : this.Details.Redirect, [
+        Validators.required,
+      ])
     );
   }
 
